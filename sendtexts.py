69990e9432f133902a590313a79bf45ce9af994f
Jaron <47575902+jaronphillips@users.noreply.github.com>
2019-02-18 09:18:35 -0700

    Add files via upload

--- sendtexts.py
+++ sendtexts.py
@@ -9,8 +9,8 @@ MinHourOfDay=8
 MaxHourOfDay=22
 
 #How frequently do you want it to send texts in minutes. It will be a random number of minutes betweemn these two settings
-MinIncriment=15
-MaxIncriment=90
+MinIncriment=30
+MaxIncriment=180
 
 #Files Used
 ListOfNumbers="PhoneNumbers.txt"
@@ -33,15 +33,18 @@ voice.login(usr,pwd)
 
 #Functions
 def Submitter():
+    logger("----------Starting Up-------------\n")
     while True:
         timer= random.randint((MinIncriment*60),(MaxIncriment*60))
         now=datetime.datetime.now().time()
+
+        logger("next message going out in " + str(round(timer/60)) + " minutes\n")
+        time.sleep(timer)        
               
         if now > datetime.time(MinHourOfDay) and now < datetime.time(MaxHourOfDay):
             for PhoneNumber in PhoneNumbers:
                 ListRandomizer(PhoneNumber)
-            logger("next message going out in " + str(timer/60) + "minutes\n")
-            time.sleep(timer)
+
 
 def ListRandomizer(PhoneNumber):
     randomizer=random.randint(0,count)
