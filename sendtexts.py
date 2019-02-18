4515a27b27550b8a0a0a8eeb8eb930616b2389da
Jaron <47575902+jaronphillips@users.noreply.github.com>
2019-02-17 23:12:56 -0700

    Add files via upload

--- sendtexts.py
+++ sendtexts.py
@@ -4,57 +4,57 @@ from googlevoice import Voice
 import datetime
 import keyring
 
+#Time of day to run. Which hours to start submitting texts and which hours to finish. 
+MinHourOfDay=8
+MaxHourOfDay=23
+
+#How frequently do you want it to send texts in minutes. It will be a random number of minutes betweemn these two settings
 MinIncriment=10
 MaxIncriment=90
 
+#Files Used
 ListOfNumbers="PhoneNumbers.txt"
 ListofSayings="Sayings.txt"
 LogFile="MotivationLog.txt"
 
-voice = Voice()
-parka=keyring.get_password('1','1')
-shmarka=keyring.get_password('2','2')
-voice.login(shmarka,parka)
-
 PhoneList=open(ListOfNumbers, "r")
 PhoneNumbers=PhoneList.read().splitlines()
 
-def randomtexter():
+#Google voice Login
+voice = Voice()
+usr=keyring.get_password('2','2')
+pwd=keyring.get_password('1','1')
+voice.login(usr,pwd)
+
+
+#Functions
+def Submitter():
     while True:
         timer= random.randint((MinIncriment*60),(MaxIncriment*60))
-             
-        Myfile=open(ListofSayings, "r")
-        sayings=Myfile.readlines()
-        count=int(len(sayings)-1)
-        randomizer=random.randint(0,count)
-        randomstring = sayings[randomizer]
-
-        nextrun=str(timer/60)
         now=datetime.datetime.now().time()
-        logtime = str(now)
-        with open(LogFile, "a") as Log:
-                Log.write(logtime + "  next run is in " + nextrun + " minutes..." + randomstring)
-                
-        time.sleep(timer)
-      
-        if now > datetime.time(8) and now < datetime.time(22):
+        
+        if now > datetime.time(MinHourOfDay) and now < datetime.time(MaxHourOfDay):
             for PhoneNumber in PhoneNumbers:
-                voice.send_sms(PhoneNumber, randomstring)
+                ListReader(PhoneNumber,timer)
+        time.sleep(timer)
 
-            now=datetime.datetime.now().time()
-            logtime = str(now)
-            with open(LogFile, "a") as Log:
-                Log.write(logtime+"  completed \n")
+def ListReader(PhoneNumber,timer):
+    Myfile=open(ListofSayings, "r")
+    sayings=Myfile.readlines()
+    count=int(len(sayings)-1)
+    randomizer=random.randint(0,count)
+    randomstring = sayings[randomizer]
+    Texter(PhoneNumber, randomstring, timer)
 
-randomtexter()
+def Texter(PhoneNumber, randomstring, timer):
+    voice.send_sms(PhoneNumber, randomstring)
+    nextrun=str(timer/60)
+    now=datetime.datetime.now().time()
+    logtime = str(now)
+    with open(LogFile, "a") as Log:
+        Log.write(logtime + "  Just sent this: "+ randomstring)
+        Log.write(logtime + "  next run is in " + nextrun + " minutes \n")
 
 
-
-
-
-
-
-
-
-
-
+#Run Main Loop
+Submitter()
